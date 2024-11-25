746f9acf35cbcde4b4b929df0e7d8889ea8c2dbe
Welepy <149401955+Welepy-dev@users.noreply.github.com>
2024-11-25 15:23:02 +0100

    Update init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,12 +17,12 @@ return {
     "github/copilot.vim",
     lazy = false,
     config = function()  -- Mapping tab is already used by NvChad
-      vim.g.copilot_no_tab_map = true;
-      vim.g.copilot_assume_mapped = true;
-      vim.g.copilot_tab_fallback = "";
+      vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+      vim.g.copilot_tab_fallback = ""
     -- The mapping is set to other key, see custom/lua/mappings
     -- or run <leader>ch to see copilot mapping section
-      vim.api.nvim_set_keymap("i", "<C-Right>", 'copilot#Accept("")', {silent = true});
+      vim.api.nvim_set_keymap("i", "<C-Right>", 'copilot#Accept("")', {silent = true})
     end
   },
   -- {
