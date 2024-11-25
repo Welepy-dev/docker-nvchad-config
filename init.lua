6427e6a680759e0790c26b1cdf97fec8dd0ffa1b
{Welepy-dev} <mwsilva.contato@gmail.com>
2024-11-25 14:47:03 +0100

    new mappings

--- init.lua
+++ init.lua
@@ -35,3 +35,54 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+
+-- General Settings
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Key Mappings
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Save with <leader>w
+map('n', '<leader>w', ':w!<CR>', opts)
+
+-- Window navigation
+map('n', '<C-j>', '<C-W>j', {})
+map('n', '<C-k>', '<C-W>k', {})
+map('n', '<C-h>', '<C-W>h', {})
+map('n', '<C-l>', '<C-W>l', {})
+
+-- Toggle line numbers
+map('n', '<F3>', ':if &number == 1 | set norelativenumber nonumber | else | set number relativenumber | endif<CR>', opts)
+
+-- NERDTree mappings (assuming Nvim-Tree or equivalent is used)
+map('n', '<leader>nn', ':NERDTreeToggle<CR>', opts)
+map('n', '<leader>nb', ':NERDTreeFromBookmark ', {})
+map('n', '<leader>nf', ':NERDTreeFind<CR>', opts)
+
+-- Move lines up and down
+map('n', '<A-Up>', ':m-2<CR>==', opts)
+map('n', '<A-Down>', ':m+1<CR>==', opts)
+map('v', '<A-Up>', ":m '<-2<CR>gv=gv", opts)
+map('v', '<A-Down>', ":m '>+1<CR>gv=gv", opts)
+
+-- Search mappings
+map('n', '<C-space>', '?', {})
+
+-- Clear search
+map('n', '<leader><CR>', ':noh<CR>', opts)
+
+-- Wrapping selection in pairs
+map('v', '(', '<esc>`>a)<esc>`<i(<esc>', opts)
+map('v', '[', '<esc>`>a]<esc>`<i[<esc>', opts)
+map('v', '{', '<esc>`>a}<esc>`<i{<esc>', opts)
+map('v', '"', '<esc>`>a"<esc>`<i"<esc>', opts)
+map('v', "'", "<esc>`>a'<esc>`<i'<esc>", opts)
+map('v', '`', '<esc>`>a`<esc>`<i`<esc>', opts)
+
+-- Telescope-like functionality (adjust based on your plugin)
+map('n', '<leader>f', ':Files<CR>', opts)  -- Requires a plugin like fzf.vim or Telescope
+map('n', '<leader>b', ':Buffers<CR>', opts)
+
